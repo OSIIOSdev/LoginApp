900c8da960599b1ba8c45666a5904692d379ff89
Ilya Osipov <osiios.dev@gmail.com>
2022-08-14 17:22:02 +0300

    1. Add showAlert 2. Add unwind

--- LoginApp/MainViewController.swift
+++ LoginApp/MainViewController.swift
@@ -4,16 +4,48 @@
 //
 //  Created by Илья on 14.08.2022.
 //
-
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    @IBOutlet var logInButton: UIButton!
+    
+    @IBOutlet var forgotNameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
+    var userName: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        userNameTF.text = userName
     }
-
+    
+    @IBAction func forgotNameButtonPressed() {
+        showAlert(with: "Oops!", and: "Your name is User 😉")
+    }
+    
+    @IBAction func forgotPasswordButtonPressed() {
+        showAlert(with: "Oops!", and: "Your password is Password 😉")
+    }
+    
+    //MARK: - UIAlertController
+    private func showAlert(with title: String, and message: String) {
+        let alertMessage = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alertMessage.addAction(okAction)
+        
+        present(alertMessage, animated: true)
+    }
+    
+    //MARK: - undiwndSegue
+    @IBAction func undiwndSegue(_ segue: UIStoryboardSegue) {
+       
+    }
 
 }
 
---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Илья on 14.08.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    @IBOutlet var welcomeMessage: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }    
+
+}
